24c7aa87059763d62a689f2d103065289a4c16cc
MattIPv4 <me@mattcowley.co.uk>
2021-01-10 23:14:25 +0000

    kaleidoscope section works

--- src/index.js
+++ src/index.js
@@ -28,11 +28,65 @@ const getSvgElm = svgString => {
     return svg;
 };
 
+const setPogGridSize = (grid, size) => {
+    // TODO: Replace with JS calc
+    const sizeStr = `min(${size}vh,${size}vw)`;
+    grid.style.width = sizeStr;
+    grid.style.minWidth = sizeStr;
+    grid.style.height = sizeStr;
+    grid.style.minHeight = sizeStr;
+};
+
+const getPogGrid = (sourceImg, zoomTransition) => {
+    const outer = document.createElement('div');
+    outer.style.width = '100vw';
+    outer.style.maxWidth = '100vw';
+    outer.style.height = '100vh';
+    outer.style.maxHeight = '100vh';
+    outer.style.overflow = 'hidden';
+    outer.style.display = 'flex';
+    outer.style.justifyContent = 'center';
+    outer.style.alignItems = 'center';
+    outer.style.position = 'absolute';
+    outer.style.top = '0';
+    outer.style.left = '0';
+
+    const wrapper = document.createElement('div');
+    outer.appendChild(wrapper);
+    wrapper.style.display = 'flex';
+    wrapper.style.flexDirection = 'column';
+    wrapper.style.justifyContent = 'space-between';
+    wrapper.style.transition = ['width', 'minWidth', 'height', 'minHeight']
+        .map(prop => `${prop} ${zoomTransition}`).join(', ') + ', transform 1350ms linear';
+    wrapper.style.transform = 'rotate(0deg)';
+    setPogGridSize(wrapper, 215);
+
+    for (let i = 0; i < 5; i++) {
+        const row = document.createElement('div');
+        wrapper.appendChild(row);
+        row.style.display = 'flex';
+        row.style.flexDirection = 'row';
+        row.style.justifyContent = 'space-between';
+        row.style.flexWrap = 'nowrap';
+
+        for (let j = 0; j < 5; j++) {
+            const img = document.createElement('img');
+            row.appendChild(img);
+            img.src = sourceImg.src;
+            img.style.transition = `transform 1000ms linear, width ${zoomTransition}, height ${zoomTransition}`;
+            img.style.transform = 'rotate(0deg)';
+            if (i === 2 && j === 2) img.style.opacity = '0'; // Hide the center
+        }
+    }
+
+    return outer;
+};
+
 const waitForFrame = () => new Promise(resolve => window.requestAnimationFrame(resolve));
 
 const waitForMillis = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-document.addEventListener('DOMContentLoaded', async () => {
+const main = async () => {
     const timingFunction = 'cubic-bezier(.7, 0, .3, 1)';
     const timingFunctionArrows = 'cubic-bezier(.5, .5, .5, .5)';
 
@@ -160,5 +214,45 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     // Poggers time (1150ms)
     await waitForMillis(50);
+    const gridZoomTransition = '350ms linear';
     pog.style.display = '';
-});
+    pog.firstElementChild.style.transition = `transform 675ms cubic-bezier(.1, 0, .9, 1), width ${gridZoomTransition}, height ${gridZoomTransition}`;
+    const grid = getPogGrid(pog.firstElementChild, gridZoomTransition);
+    document.body.appendChild(grid);
+    await waitForFrame();
+    await waitForFrame();
+    pog.firstElementChild.style.transform = 'rotate(-45deg)'; // Ends at 1825ms
+    grid.firstElementChild.style.transform = 'rotate(100deg)'; // Ends at 2500ms
+
+    // Zoom out (1500ms)
+    await waitForMillis(350);
+    setPogGridSize(grid.firstElementChild, 150);
+    for (const img of grid.firstElementChild.getElementsByTagName('img')) {
+        img.style.width = '6em';
+        img.style.height = '6em';
+        img.style.transform = 'rotate(100deg)';
+    }
+    pog.firstElementChild.style.width = '6em';
+    pog.firstElementChild.style.height = '6em';
+
+    // Center rotation back to normal (1825ms)
+    await waitForMillis(325);
+    pog.firstElementChild.style.transform = 'rotate(0deg)'; // Ends at 2500ms
+
+    // Zoom in and solo (2500ms)
+    await waitForMillis(675);
+    document.body.style.background = '';
+    grid.style.display = 'none';
+    pog.firstElementChild.style.transition = `width 500ms ${timingFunction}, height 500ms ${timingFunction}`; // Ends at 3000ms
+    await waitForFrame();
+    pog.firstElementChild.style.width = '10em';
+    pog.firstElementChild.style.height = '10em';
+
+    // TODO: Zoom back out
+
+    // TODO: Show Glitch w/ blink
+
+    // TODO: Show PogChamp again
+};
+
+document.addEventListener('DOMContentLoaded', main);
